3878707db00bcb45b822245dcd44a8d24c1071cd
Nikas <d1jp@TECHIN.INTERNAL>
2025-11-25 08:48:25 +0200

    Begin

---
+++ reduce_method/reduce_method/task1.js
@@ -0,0 +1,4 @@
+function total(arr) {
+  return arr.reduce((x, y) => x + y);
+}
+console.log(total([1, 2, 3]));
---
+++ reduce_method/reduce_method/task2.js
@@ -0,0 +1,4 @@
+function stringConcat(arr) {
+    return arr.join("")
+}
+console.log(stringConcat([1,2,3]));
