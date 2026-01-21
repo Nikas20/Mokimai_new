38af4a9792ce837fc37d9e1092ac4faeacd5ec18
Nikas20 <simatolskisnikas@gmail.com>
2026-01-21 08:24:57 +0200

    Fix bug

--- react14/src/components/MovieEdit.jsx
+++ react14/src/components/MovieEdit.jsx
@@ -53,6 +53,7 @@ function MovieEdit({movie, fetchData,closeModal}) {
 
   return (
     <>
+    {errors}
       <form onSubmit={handleSubmit(formSubmitHandler)}>
         <div className="grid grid-cols-4 gap-4">
           <div>
