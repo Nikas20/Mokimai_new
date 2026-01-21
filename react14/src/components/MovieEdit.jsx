314c44a4c868658e60665dc64b2d8c300993e0dd
Nikas20 <simatolskisnikas@gmail.com>
2026-01-21 08:24:18 +0200

    Complite modal

--- react14/src/components/MovieEdit.jsx
+++ react14/src/components/MovieEdit.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-function MovieEdit({movie, fetchData}) {
+function MovieEdit({movie, fetchData,closeModal}) {
     const {id, title, genre, year, rating} = movie
     const {
     register,
@@ -34,7 +34,8 @@ function MovieEdit({movie, fetchData}) {
       if (response.ok) {
         alert("Success");
         reset();
-
+        fetchData();
+        closeModal();
       } else {
         throw Error("eerro");
       }
