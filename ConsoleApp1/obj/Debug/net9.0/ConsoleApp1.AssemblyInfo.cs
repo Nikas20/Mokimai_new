da07696a0f1b4c0ebe5076d7fb7e5b6aaedebfe3
unknown <simatolskisnikas@gmail.com>
2026-03-28 16:56:20 +0200

    f

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bb10ff919f5eee9069960b93e9cbd760f300c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e2b0eb02088ea666126f2bffdb8fafd42a5542")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
